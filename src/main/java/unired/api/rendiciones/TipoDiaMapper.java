1bcd46a074d1116fdc7b33dd6aed2a7f5d369c8f
Jorge Silva Borda <jorge.silva8686@gmail.com>
2022-09-11 16:10:55 -0300

    Se ajusta la query para ver el tipo día

--- src/main/java/unired/api/rendiciones/TipoDiaMapper.java
+++ src/main/java/unired/api/rendiciones/TipoDiaMapper.java
@@ -10,39 +10,44 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface TipoDiaMapper {
+
     /*
     @ConstructorArgs({
 	@Arg(name = "feriado", column = "feriado", javaType = java.lang.Integer.class),
 	@Arg(name = "finSemana", column = "finSemana", javaType = java.lang.Integer.class),
 	@Arg(name = "ayerFeriado", column = "ayerFeriado", javaType = java.lang.Integer.class)
     })
-    */
-    /*
+     */
+ /*
     @Results({
 	@Result(property = "feriado", column = "feriado", javaType = java.lang.Integer.class),
 	@Result(property = "finSemana", column = "finSemana", javaType = java.lang.Integer.class),
 	@Result(property = "ayerFeriado", column = "ayerFeriado", javaType = java.lang.Integer.class)
     })
     @ResultType(TipoDia.class)
-    */
+     */
     @Select("SELECT\n"
-	    + "	SUM(\n"
-	    + "		CASE\n"
-	    + "			WHEN CONVERT(DATE, GETDATE()) = CONVERT(DATE, FechaFeriado) THEN 1 ELSE 0\n"
-	    + "		END\n"
-	    + "	    ) as feriado,\n"
-	    + "	CASE\n"
-	    + "	    WHEN DATEPART(WEEKDAY, GETDATE()) IN (7, 1) THEN 1 ELSE 0\n"
-	    + "	END as finSemana,\n"
-	    + "	CASE\n"
-	    + "	    WHEN DATEPART(WEEKDAY, DATEADD(DAY, -1, CONVERT(DATE,GETDATE()))) = 1 "
-	    + "		OR "
-	    + "	    SUM("
-	    + "		CASE "
-	    + "		    WHEN DATEADD(DAY, -1, CONVERT(DATE, GETDATE())) = CONVERT(DATE, FechaFeriado) THEN 1 ELSE 0\n"
-	    + "		END \n"
-	    + "		) > 0 THEN 1 ELSE 0\n"
-	    + "	END as ayerFeriado\n"
-	    + "FROM  Feriados")
+	    + "	    	SUM(\n"
+	    + "	    		CASE\n"
+	    + "	    			WHEN CONVERT(DATE, GETDATE()) = CONVERT(DATE, FechaFeriado) THEN 1 ELSE 0\n"
+	    + "	    		END\n"
+	    + "	    	    ) as feriado,\n"
+	    + "	    	    \n"
+	    + "	    	CASE\n"
+	    + "	    	    WHEN DATEPART(WEEKDAY, GETDATE()) IN (7, 1) THEN 1 ELSE 0\n"
+	    + "	    	END as finSemana,\n"
+	    + "	    	\n"
+	    + "	    	CASE\n"
+	    + "	    	    WHEN DATEPART(WEEKDAY, DATEADD(DAY, -1, CONVERT(DATE,GETDATE()))) = 1 \n"
+	    + "	    			OR \n"
+	    + "	    			DATEPART(WEEKDAY, DATEADD(DAY, -1, CONVERT(DATE,GETDATE()))) = 7\n"
+	    + "	    			OR\n"
+	    + "	    	    SUM(\n"
+	    + "	    		CASE \n"
+	    + "	    		    WHEN DATEADD(DAY, -1, CONVERT(DATE, GETDATE())) = CONVERT(DATE, FechaFeriado) THEN 1 ELSE 0\n"
+	    + "	    		END \n"
+	    + "	    		) > 0 THEN 1 ELSE 0\n"
+	    + "	    	END as ayerFeriado\n"
+	    + "	    FROM  Feriados")
     TipoDia obtenerTipoDiaHoy();
 }
